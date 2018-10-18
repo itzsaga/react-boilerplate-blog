8a3529d4fb697dac4b8b89b5726d5fb5af1939da
Seth <sethalexander@pm.me>
2018-10-17 20:29:26 -0500

    Fix path on exclude

--- webpack.common.js
+++ webpack.common.js
@@ -14,7 +14,7 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        exclude: ['node_modules'],
+        exclude: ['/node_modules'],
         use: [{ loader: 'babel-loader' }],
       },
       {
